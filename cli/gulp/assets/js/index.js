f0e4fd6785fc85e7d18e3868ca909e62a683389c
kepo4ka <konstantin.bugulov@mail.ru>
2019-07-15 18:03:05 +0300

    test

--- cli/gulp/assets/js/index.js
+++ cli/gulp/assets/js/index.js
@@ -1,10 +1,23 @@
 import * as test from "./export";
-window.jQuery = window.$ = require('jquery');
-
-
-
 // alert( `${test.one} and ${test.two}` ); // 1 and 2
 
+window.jQuery = window.$ = require('jquery');
 $(function () {
     console.log('Hello jQuery');
-});
+})
+;
+
+
+import Vue from 'vue';
+
+new Vue({
+    data: {
+        content: 'test'
+    },
+    computed: {
+        test: function () {
+            return 123;
+        }
+    }
+}).$mount('#app');
+
