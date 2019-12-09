e59cd39446c187bcf6d09b5ff08b760844f56604
kepo4ka <konstantin.bugulov@mail.ru>
2019-12-09 18:23:03 +0300

    mysql

---
+++ mysql/db_get.bat
@@ -0,0 +1,2 @@
+mysqldump -u root -p meat_king > meat_king.sql
+echo Complete!
---
+++ mysql/db_set.bat
@@ -0,0 +1,12 @@
+SET table=meat_king
+echo %table%
+
+@ECHO OFF
+echo Droping...
+echo Y | mysqladmin -h localhost -u root drop %table%
+echo Creating...
+echo Y | mysqladmin -h localhost -u root create %table%
+echo Importing...
+mysql -u root %table% < %table%.sql
+echo Complete!
+pause
